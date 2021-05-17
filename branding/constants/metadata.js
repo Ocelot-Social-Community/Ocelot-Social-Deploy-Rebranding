8656f6f65c2a2b7c04b03ea9a24df5e54190a1b9
Wolfgang Huß <wolle.huss@pjannto.com>
2021-05-17 14:24:39 +0200

    Adjust package.json and metadata.js to sender.fm

--- branding/constants/metadata.js
+++ branding/constants/metadata.js
@@ -1,8 +1,8 @@
 export default {
-  APPLICATION_NAME: 'ocelot.social',
-  APPLICATION_SHORT_NAME: 'ocelot',
-  APPLICATION_DESCRIPTION: 'Ocelot Social Community',
-  COOKIE_NAME: 'ocelot-social-token',
-  ORGANIZATION_NAME: 'busFaktor e.V.',
-  ORGANIZATION_JURISDICTION: 'Somewhere',
+  APPLICATION_NAME: 'sender.fm-ocelot.social',
+  APPLICATION_SHORT_NAME: 'sender.fm',
+  APPLICATION_DESCRIPTION: 'sender.fm ocelot.social Network',
+  COOKIE_NAME: 'sender-fm-ocelot-social-token',
+  ORGANIZATION_NAME: 'sender.fm',
+  ORGANIZATION_JURISDICTION: 'Österreich',
 }
