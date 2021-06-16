1d8233d9e075bb06309fc46d5b3e5aa07ff2931f
Wolfgang Huß <wolle.huss@pjannto.com>
2021-06-16 10:31:25 +0200

    Change next configurations

--- branding/constants/metadata.js
+++ branding/constants/metadata.js
@@ -1,9 +1,9 @@
 // this file is duplicated in `backend/src/config/metadata.js` and `webapp/constants/metadata.js` and replaced on rebranding
 export default {
-  APPLICATION_NAME: 'ocelot.social',
-  APPLICATION_SHORT_NAME: 'ocelot.social',
-  APPLICATION_DESCRIPTION: 'Ocelot Social Community',
+  APPLICATION_NAME: 'helfa.social',
+  APPLICATION_SHORT_NAME: 'helfa.social',
+  APPLICATION_DESCRIPTION: 'H.e.l.f.a. Community Network driven by ocelot.social',
   COOKIE_NAME: 'ocelot-social-token',
-  ORGANIZATION_NAME: 'busFaktor e.V.',
+  ORGANIZATION_NAME: 'H.e.l.f.a. Community',
   ORGANIZATION_JURISDICTION: 'Deutschland',
 }
