4b7b284625ed641643ee518c5b9d83c96fca12f4
Wolfgang Huß <wolle.huss@pjannto.com>
2021-10-14 11:07:33 +0200

    Set correct e-mails

--- branding/constants/emails.js
+++ branding/constants/emails.js
@@ -1,7 +1,7 @@
 // this file is duplicated in `backend/src/config/` and `webapp/constants/` and replaced on rebranding by https://github.com/Ocelot-Social-Community/Ocelot-Social-Deploy-Rebranding/tree/master/branding/constants/
 export default {
-  SUPPORT_EMAIL: 'hello@ocelot.social',
-  MODERATION_EMAIL: 'hello@ocelot.social',
+  SUPPORT_EMAIL: 'support@helfa.social',
+  MODERATION_EMAIL: 'support@helfa.social',
   // ATTENTION: the following links have to be defined even for internal pages with full URLs as example like 'https://staging.ocelot.social/support', because they are used in e-mails!
   ORGANIZATION_LINK: 'https://helfa.social/organization',
   SUPPORT_LINK: 'https://helfa.social/support',
